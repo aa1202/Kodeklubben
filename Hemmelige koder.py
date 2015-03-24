a4e07359971d9959167a45eb188c66fc7e27280d
aa1202 <andreas.amundsen123@gmail.com>
2015-03-24 15:28:23 +0100

    Modifications to Hemmelige Koder.py

--- Hemmelige koder.py
+++ Hemmelige koder.py
@@ -1,51 +1,51 @@
 __author__ = 'andram1202'
 alphabet = "abcdefghijklmnopqrstuvwxyzæøå"
 
-#Encodes a single letter
+# Encodes a single letter
 def encode_letter(letter, secret):
     pos = alphabet.find(letter)
     new_pos = (pos + secret)
-
     if new_pos >= 29:
-        new_pos = new_pos - 29
-
+        new_pos -= 29
     return alphabet[new_pos]
 
-#Decodes a single letter
+
+# Decodes a single letter
 def decode_letter(letter, secret):
     pos = alphabet.find(letter)
     new_pos = (pos - secret)
-
     if new_pos < 0:
-        new_pos = new_pos + 29
-
+        new_pos += 29
     return alphabet[new_pos]
 
-#Encodes a message
+
+# Encodes a message
 def encode_msg(message, secret):
     encrypted_message = ""
     for character in message:
         if character in alphabet:
-            encrypted_message = encrypted_message +  encode_letter(character, secret)
+            encrypted_message += encode_letter(character, secret)
         else:
-            encrypted_message = encrypted_message + character
-    print("Decryped message -", encrypted_message)
+            # Cheks for whitespace
+            encrypted_message += character
+    print("Decrypted message -", encrypted_message)
 
-#Decodes an unknown message when the secret is known
+
+# Decodes an unknown message when the secret is known
 def decode_msg(message, secret):
     decrypted_message = ""
     for character in message:
         if character in alphabet:
-            decrypted_message = decrypted_message + decode_letter(character, secret)
+            decrypted_message += decode_letter(character, secret)
         else:
-            decrypted_message = decrypted_message + character
+            # Cheks for whitespace
+            decrypted_message += character
     print("Encrypted message -", decrypted_message)
 
-#Decodes an encrypted message when the secret is unknown - Therefore returns many decoded strings
+
+# Decodes an encrypted message when the secret is unknown - Therefore returns many decoded strings
 def decode_unknown(message, secret):
     for secret in range(secret):
         decode_msg(message, secret)
         secret -= 1
-
-
 
